3c9efa382a8a247edbfea9d77a12724ff727a696
Nitro <nitro0@naver.com>
2020-12-13 13:33:18 +0900

    Update UI

--- emoji/src/components/module/Nav/Nav.js
+++ emoji/src/components/module/Nav/Nav.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Item from './NavItem'
+import GIF from '../../../assets/img/GIF.svg'
+import Setting from '../../../assets/img/Setting.svg'
 
 class Nav extends React.Component {
+
+    importAll = (r) => {
+        return r.keys().map(r);
+    }
+
     render() {
+        const emojis = this.importAll(require.context('../../../assets/img/emoji', false, /\.(png|jpe?g|svg)$/))
+        const conditional = Math.floor(Math.random() * Object.keys(emojis).length);
+        const emoji = emojis[conditional]
+
         return (
             <div className='nav'>
-                <Item index={0} img='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/200px-Noto_Emoji_KitKat_263a.svg.png' />
-                <Item index={1} img='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/200px-Noto_Emoji_KitKat_263a.svg.png' />
-                <Item index={2} img='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/200px-Noto_Emoji_KitKat_263a.svg.png' />
+                <Item index={0} img={emoji} />
+                <Item index={1} img={GIF} />
+                <Item index={2} img={Setting} />
             </div>
         )
     }
